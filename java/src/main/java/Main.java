2692f000efed69eed29d2455ec3af996e99cc66a
Feamor <feamor.web@gmail.com>
2019-11-26 13:01:04 +0300

    Initial commit

---
+++ java/src/main/java/Main.java
@@ -0,0 +1,8 @@
+import sun.awt.AppContext;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!!!");
+    System.out.println(AppContext.getAppContext());
+  }
+}
